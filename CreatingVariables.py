97046e7bdf3c9a1f157699dacca76c08f7675f5a
KeremErkut <kciftlikci@gmail.com>
2025-01-30 18:23:55 +0300

    If structure and lower and upper methods added.

--- CreatingVariables.py
+++ CreatingVariables.py
@@ -7,6 +7,16 @@ print (x * y)
 k = len(a + b + c)
 print(k)
 
+upperTest = "i need to convert this to uppers."
+
+if(upperTest.islower):
+    print("This sentence is upper")
+print(upperTest.upper())
 
 
+lowerTest = "I NEED TO CONVERT THIS TO LOWERS."
+
+if(lowerTest.isupper()):
+    print("The sentence is lower")
+print(lowerTest.lower())
 
