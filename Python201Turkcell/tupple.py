9d1d1837e2aaff80da61ba2bc9b421216ff8cf96
KeremErkut <kciftlikci@gmail.com>
2025-03-04 14:52:22 +0300

    Tuples are ended.

--- Python201Turkcell/tupple.py
+++ Python201Turkcell/tupple.py
@@ -1,1 +1,10 @@
-""
+t = ("kerem","erkut",1,2,3,5.7,["ali",9,8,7])
+t = "kerem","erkut",1,2,3,5.7,["ali",9,8,7]
+
+k = ("John",) #Don't forget to add a comma at the end to recognize the tuple data type.
+print(type(k))
+
+testTuple = (1, 2, 3, 4, 5)
+
+testTuple[1]
+testTuple[0:3]
