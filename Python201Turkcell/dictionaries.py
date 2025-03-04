33d7d161a3ddd35c67d9861d10e4a51cfe4f88e7
KeremErkut <kciftlikci@gmail.com>
2025-03-04 15:09:10 +0300

    dictionaries ended

---
+++ Python201Turkcell/dictionaries.py
@@ -0,0 +1,6 @@
+dictionary = {"REG" : ["Regression Model", 10],
+              "LOJ" : ["Logistics Model", 20],
+              "CART" : ["Classification and Reg", 30]}
+
+print (dictionary)
+print(dictionary["REG"])
