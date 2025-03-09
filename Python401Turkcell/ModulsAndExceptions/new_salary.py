74e1769ca346dd93b0ce3387c79061fdf72ee361
kerem <kciftlikci@gmail.com>
2025-03-09 21:09:52 +0300

    modulsAndExceptions.ipynb is completed.

---
+++ Python401Turkcell/ModulsAndExceptions/new_salary.py
@@ -0,0 +1,6 @@
+def newSalary(current_salary):
+    # Example calculation for new salary
+    increase_rate = 0.1  # 10% increase
+    new_salary = current_salary * (1 + increase_rate)
+    return new_salary
+
